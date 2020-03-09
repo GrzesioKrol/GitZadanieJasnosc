be60f993b16837e434f2f483bae5cedf9db8b6f7
Grzegorz Wojtiuk <grzesio.woj98@gmail.com>
2020-03-09 14:57:19 +0100

    zad7

--- jasnosc/jasnosc/Form1.cs
+++ jasnosc/jasnosc/Form1.cs
@@ -65,47 +65,93 @@ namespace jasnosc
 
         private void bw_DoWork(object sender, DoWorkEventArgs e)
         {
+            double report, report2=0;
+            report = Math.Abs(100 / progress);
+            if (progress > 0)
+            {
+                for (int j = 0; j < progress; j++)
+                {
+                    if (bw.CancellationPending)
+                    {
+                        e.Cancel = true;
+                    }
+                    else
+                    {
+                        b = (float)j / 255.0f;
+                        ColorMatrix cm = new ColorMatrix(new float[][]
+                            {
+                    new float[] {1, 0, 0, 0, 0},
+                    new float[] {0, 1, 0, 0, 0},
+                    new float[] {0, 0, 1, 0, 0},
+                    new float[] {0, 0, 0, 1, 0},
+                    new float[] {b, b, b, 1, 1},
+                            });
+                        ImageAttributes attributes = new ImageAttributes();
+                        attributes.SetColorMatrix(cm);
+                        Point[] points =
+                            {
+                    new Point(0, 0),
+                    new Point(image.Width, 0),
+                    new Point(0, image.Height),
+                    };
+                        Rectangle rect = new Rectangle(0, 0, image.Width, image.Height);
+                        bm = new Bitmap(image.Width, image.Height);
+                        using (Graphics gr = Graphics.FromImage(bm))
+                        {
+                            gr.DrawImage(image, points, rect,
+                            GraphicsUnit.Pixel, attributes);
+                        }
+                        obrazek.Image = bm;
 
-            b = (float)progress / 255.0f;
-            ColorMatrix cm = new ColorMatrix(new float[][]
-                {
-                new float[] {1, 0, 0, 0, 0},
-                new float[] {0, 1, 0, 0, 0},
-                new float[] {0, 0, 1, 0, 0},
-                new float[] {0, 0, 0, 1, 0},
-                new float[] {b, b, b, 1, 1},
-                });
-            ImageAttributes attributes = new ImageAttributes();
-            attributes.SetColorMatrix(cm);
-            Point[] points =
-                {
-                new Point(0, 0),
-                new Point(image.Width, 0),
-                new Point(0, image.Height),
-                };
-            Rectangle rect = new Rectangle(0, 0, image.Width, image.Height);
-            bm = new Bitmap(image.Width, image.Height);
-            using (Graphics gr = Graphics.FromImage(bm))
-            {
-                gr.DrawImage(image, points, rect,
-                GraphicsUnit.Pixel, attributes);
-            }
-            for (int i = 0; i < 11; i++)
-            {
-                if (bw.CancellationPending)
-                {
-                    e.Cancel = true;
+                        report2 += report;
+                        bw.ReportProgress((int)report2);
+                    }
                 }
-                else
+            } else
+            {
+                for (int j = 0; j > progress; j--)
                 {
-                    Thread.Sleep(100);
-                    bw.ReportProgress(i*10);                                       
+                    if (bw.CancellationPending)
+                    {
+                        e.Cancel = true;
+                    }
+                    else
+                    {
+                        b = (float)j / 255.0f;
+                        ColorMatrix cm = new ColorMatrix(new float[][]
+                            {
+                    new float[] {1, 0, 0, 0, 0},
+                    new float[] {0, 1, 0, 0, 0},
+                    new float[] {0, 0, 1, 0, 0},
+                    new float[] {0, 0, 0, 1, 0},
+                    new float[] {b, b, b, 1, 1},
+                            });
+                        ImageAttributes attributes = new ImageAttributes();
+                        attributes.SetColorMatrix(cm);
+                        Point[] points =
+                            {
+                    new Point(0, 0),
+                    new Point(image.Width, 0),
+                    new Point(0, image.Height),
+                    };
+                        Rectangle rect = new Rectangle(0, 0, image.Width, image.Height);
+                        bm = new Bitmap(image.Width, image.Height);
+                        using (Graphics gr = Graphics.FromImage(bm))
+                        {
+                            gr.DrawImage(image, points, rect,
+                            GraphicsUnit.Pixel, attributes);
+                        }
+                        obrazek.Image = bm;
+
+                        report2 += report;
+                        bw.ReportProgress((int)report2);
+                    }
                 }
             }
         }
         private void bw_ProgressChanged(object sender, ProgressChangedEventArgs e)
-        {
-            progressBar.Value = e.ProgressPercentage;           
+        {            
+             progressBar.Value = e.ProgressPercentage;           
         }
 
         private void bw_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
